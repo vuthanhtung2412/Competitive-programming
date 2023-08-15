de4a40879425271f1693f4a4b39ecf43d2ddb1f7
vuthanhtung <vuthanhtung2016hn@gmail.com>
2023-08-16 00:39:40 +0200

    Valid Parenthese

---
+++ neetCode2023/Stack/ValidParentheses.py
@@ -0,0 +1,21 @@
+def isValid(s: str) -> bool:
+    opens = {'(', '{', '['}
+    closes = {')', '}', ']'}
+    stack = []
+    for c in s:
+        if not stack and c in closes:
+            return False
+        else:
+            if c in opens:
+                stack.append(c)
+            else:
+                if c == ')' and stack[-1] == '(':
+                    stack.pop()
+                elif c == '}' and stack[-1] == '{':
+                    stack.pop()
+                elif c == ']' and stack[-1] == '[':
+                    stack.pop()
+                else:
+                    return False
+
+    return not stack

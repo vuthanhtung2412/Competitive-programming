893bba64d4f7329d2b7222c4417fc9859ddf69c2
vuthanhtung <vuthanhtung2016hn@gmail.com>
2023-08-21 18:46:40 +0200

    faster solution for car fleet

--- neetCode2023/Stack/car fleet.py
+++ neetCode2023/Stack/car fleet.py
@@ -1,12 +1,12 @@
 from typing import List
 
 
-def carFleet(target: int, position: List[int], speed: List[int]) -> int:
+def carFleet1(target: int, position: List[int], speed: List[int]) -> int:
     # LIFO conditional
     # conditions out:
     # 1.take more time to complete the track
     # 2.start closer to the target
-    # 3.Keep track of only the longest time won't work consider the seq of time (1321) there will be 3 fleet
+
     l = []
     le = len(position)
     for i in range(le):
@@ -26,4 +26,38 @@ def carFleet(target: int, position: List[int], speed: List[int]) -> int:
     return res
 
 
+def carFleet2(target: int, position: List[int], speed: List[int]) -> int:
+    # Only keep track of the car on top of the fleet
+    # conditions:
+    # 1.take more time to complete the track
+    # 2.start closer to the target
+
+    l = [(p, s) for p, s in zip(position, speed)]
+    l = sorted(l, key=lambda x: x[0])
+    print(l)
+
+    curr = 0
+    res = 0
+    for i in reversed(range(len(l))):
+        if (target - l[i][0]) / l[i][1] > curr:
+            curr = (target - l[i][0]) / l[i][1]
+            res += 1
+
+    return res
+
+
+def CarFleet3(target: int, position: List[int], speed: List[int]) -> int:
+    # Big O based on the size of target
+    l = [-1]*target
+    for i in range(len(position)):
+        l[position[i]] = i
+    curr = 0
+    res = 0
+    for i in reversed(range(target)):
+        tmp = (target - position[l[i]]) / speed[l[i]]
+        if l[i] != -1 and tmp > curr:
+            curr = tmp
+            res += 1
+
+    return res
 

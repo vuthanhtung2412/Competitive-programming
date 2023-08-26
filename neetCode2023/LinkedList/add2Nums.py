2781064d9f7ac87b00056dfc9e856c53aa26c06b
vuthanhtung <vuthanhtung2016hn@gmail.com>
2023-08-26 19:31:00 +0200

    add 2 nums w linked list

---
+++ neetCode2023/LinkedList/add2Nums.py
@@ -0,0 +1,54 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    plusOne = 0
+    h1 = l1
+    h2 = l2
+    res = curr = ListNode()
+    tmp = 0
+
+    while h1 is not None and h2 is not None:
+        tmp = h1.val + h2.val + plusOne
+        if tmp >= 10:
+            plusOne = 1
+            curr.next = ListNode(tmp - 10)
+        else:
+            plusOne = 0
+            curr.next = ListNode(tmp)
+
+        curr = curr.next
+        h1 = h1.next
+        h2 = h2.next
+
+    while h1:
+        tmp = h1.val + plusOne
+        if tmp >= 10:
+            plusOne = 1
+            curr.next = ListNode(tmp - 10)
+        else:
+            plusOne = 0
+            curr.next = ListNode(tmp)
+
+        curr = curr.next
+        h1 = h1.next
+
+    while h2:
+        tmp = h2.val + plusOne
+        if tmp >= 10:
+            plusOne = 1
+            curr.next = ListNode(tmp - 10)
+        else:
+            plusOne = 0
+            curr.next = ListNode(tmp)
+
+        curr = curr.next
+        h2 = h2.next
+
+    if plusOne:
+        curr.next = ListNode(1)
+
+    return res.next

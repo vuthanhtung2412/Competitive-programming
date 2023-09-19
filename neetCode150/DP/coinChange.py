5293b28eb4ec0894c848817bc2f4f4cab6b8dc1b
vuthanhtung <vuthanhtung2016hn@gmail.com>
2023-09-19 17:47:39 +0200

    coin change neetcode solution

--- neetCode150/DP/coinChange.py
+++ neetCode150/DP/coinChange.py
@@ -2,6 +2,9 @@ from typing import List
 
 
 def coinChange(coins: List[int], amount: int) -> int:
+    # first thought : can we be greedy (counter proof with example [1,3,4,5] amount = 7)
+    # second thought : DFS + backtracking
+    # memoization to reduce number of repeated calculation
     d = {}
     for c in coins:
         d[c] = 1
@@ -26,3 +29,19 @@ def coinChange(coins: List[int], amount: int) -> int:
                 return -1
 
     return minCoin(amount)
+
+def NCCoinChange(coins: List[int], amount: int) -> int:
+    # bottom up dynamic programing aproach, keep track of 0 -> amount
+    if amount == 0:
+        return 0
+    dp = [-1] * (amount+1)
+    dp[0] = 0
+    for i in range(1,amount+1):
+        for c in coins:
+            if i-c >= 0 and dp[i-c] != -1:
+                if dp[i] == -1:
+                    dp[i] = dp[i-c] + 1
+                elif dp[i] > dp[i-c] + 1:
+                    dp[i] = dp[i-c] + 1
+    print(dp)
+    return dp[-1]

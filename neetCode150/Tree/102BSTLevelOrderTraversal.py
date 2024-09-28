586940f40ad38b13b4bdcc73072bd3fe4c581b84
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-09-28 23:19:07 +0200

    ignore pycache

--- neetCode150/Tree/102BSTLevelOrderTraversal.py
+++ neetCode150/Tree/102BSTLevelOrderTraversal.py
@@ -3,14 +3,14 @@ import unittest
 from parameterized import parameterized
 from helperFunc import TreeNode, sorted_list_to_bst
 
-class TestMyFunction(unittest.TestCase):
+class Test(unittest.TestCase):
     @parameterized.expand([
         [[3,9,20,None,None,15,7], [[3],[9,20],[15,7]]],
         [[1], [[1]]],
         [[], []]
     ])
-    def test_level_order(self, sortedList, expected):
-        self.assertEqual(levelOrder(sorted_list_to_bst(sortedList)), expected)
+    def test_level_order(self, list, expected):
+        self.assertEqual(levelOrder(sorted_list_to_bst(list)), expected)
 
 def levelOrder(root: Optional[TreeNode]) -> List[List[int]]:
     return list()
